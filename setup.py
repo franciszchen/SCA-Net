cfeaac6fe0a8fc2b7fba4e5d1f4a097f0c13c731
Francis Chen <honestmanfrancis@gmail.com>
2023-10-07 10:37:54 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+# 与luo完全相同
+
+import setuptools
+
+setuptools.setup(
+    name="captioning",
+    version="0.0.1",
+    author="Ruotian Luo",
+    author_email="rluo@ttic.edu",
+    packages=setuptools.find_packages(),
+)
